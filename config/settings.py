74a076548506f4bdc40849b7c31c0132cbf233b9
Brian Sato <brian.sato1985@gmail.com>
2026-03-20 22:49:23 +0900

    Activate DEBUG for test in Render

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-sxg)=-eq2m&!^r6@8m+s46ms-03_%kq$)4#=r%pp6%(9^%)=is'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
