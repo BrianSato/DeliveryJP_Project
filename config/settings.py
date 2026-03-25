c8444eb33983ef2d3b85a497486d614055d4facc
Brian Sato <brian.sato1985@gmail.com>
2026-03-26 07:43:22 +0900

    chore: configure environment-based DEBUG and production statuc handling

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback_local_key')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
 ALLOWED_HOSTS = ['*']
 
 
@@ -131,21 +128,20 @@ USE_I18N = True
 
 USE_TZ = True
 
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
-
-
 STATIC_URL = '/static/'
-
 STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles')
 
-
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.StaticFilesStorage",
-    },
-}
+if not DEBUG:
+    STORAGES = {
+        "staticfiles": {
+            "BACKEND": "whitenoise.storage.StaticFilesStorage",
+        },
+    }
 
 LOGIN_REDIRECT_URL = '/'
 
