12167e5bd4569fa18c9993d7712649846ef2a846
Brian Sato <brian.sato1985@gmail.com>
2026-04-06 05:02:59 +0900

    update PONTOS_POR_CUPOM

--- loja/models.py
+++ loja/models.py
@@ -9,7 +9,7 @@ from loja.utils import baixar_estoque
 
 #==================== CONSTANTES =======================
 VALOR_POR_PONTO = 1000
-PONTOS_POR_CUPOM = 60
+PONTOS_POR_CUPOM = 50
 VALOR_CUPOM = 1000
 #===================== CLIENTES =========================
 class Cliente(models.Model):
