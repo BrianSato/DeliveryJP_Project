d1c9dbb91d7ce73555a81ecd4380e67af3c6786c
Brian Sato <brian.sato1985@gmail.com>
2026-04-26 19:40:58 +0900

    Cria superuser automático para Render

--- config/settings.py
+++ config/settings.py
@@ -94,7 +94,8 @@ else:
     print('USANDO RENDER')
     DATABASES = {
         'default': dj_database_url.config(
-            default=os.environ.get('DATABASE_URL'),
+            default=os.getenv('DATABASE_URL'),
+            conn_max_age=600,
         )
     }
 
--- config/urls.py
+++ config/urls.py
@@ -21,3 +21,14 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('loja.urls')),
 ]
+
+from django.contrib.auth.models import User
+
+def create_admin():
+    if not User.objects.filter(username="admin").exists():
+        User.objects.create_superuser(
+            username="admin",
+            password="admin1234"
+        )
+
+create_admin()
